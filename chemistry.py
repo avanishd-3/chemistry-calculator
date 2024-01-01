e113a5e40ffa8c248552f48defdf7a40d7283933
avanishd-3 <avanishdavuluri@gmail.com>
2023-12-31 17:24:57 -0800

    Automated help command printing

--- chemistry.py
+++ chemistry.py
@@ -220,22 +220,16 @@ def main() -> None:
                         print(_convert_dict_to_dataframe(stoich_dict).set_index('compound'))
 
             case 'h' | 'help':
-                print("""
-                1. b or balance -> balance rxn
-                2. l or limiting or limiting reagent -> find limiting reagent in rxn
-                3. c or combustion -> perform combustion analysis on combustion rxn
-                4. a or acid or acidity -> find pH, pOH, [H+], [OH-], acidity status
-                5. w or wave or waves -> find wavelength, frequency, energy of wave
-                6. v or vespr or vsepr -> calculate vsepr information based on name of molecule
-                7. d or draw -> Draw molecule using RDkit
-                8. e or electrolysis or electrochem -> Find cell, cathode, anode, cell potential
-                9. t or titration -> Perform titration given acid volume, molarity, k value and base molarity
-                10. p or property or properties -> Return atomic radius, electronegativity, first ionization 
-                    of elements given
-                11. s or stoich or stoichiometry -> Return grams, moles, molecules, molar mass for an amount of 
-                    a compound
-                12. q or quit -> Quit application
-                """)
+                with open('README.md') as f:
+                    lines: list = f.readlines()
+                    command = lines.index('Commands:\n')
+                    command_lines = lines[command+2:-1]  # Excludes help command from being printed
+                    removed_space_lines = [line[:-1] for line in command_lines]
+
+                    for line in removed_space_lines:
+                        print(f'\t\t\t\t{line}')
+
+                f.close()
 
             case 'q' | 'quit':
                 break
